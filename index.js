3a634f903457ceed438d5c950e735e116728b7d6
rappeu <raphael_peuckert@icloud.com>
2022-03-16 09:02:36 +0100

    implemented the hamburger menu function, have to make the next section of the site next

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const hamburgerMenu = document.querySelector('#hamburgerMenu');
+const hamburgerNav = document.querySelector('#hamburgerNav');
+const body = document.querySelector('body');
 let menuOn = false;
 hamburgerMenu.addEventListener('click', () => {
     if(!menuOn) {
         hamburgerMenu.classList.add('open');
         menuOn = true;
+        hamburgerNav.classList.add('open');
     } else {
         hamburgerMenu.classList.remove('open');
         menuOn = false;
+        hamburgerNav.classList.remove('open');
     }
 })
+
+window.onresize = function() {
+    if(window.innerWidth >= 1000) {
+        hamburgerMenu.classList.remove('open');
+        hamburgerNav.classList.remove('open');
+        menuOn = false;
+    }
+}
